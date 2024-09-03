1db80d3a48f0f1733ab040e5ed77341d75f5963d
Raúl Soto <rssotovejar@gmail.com>
2024-09-03 13:30:08 -0700

    Homepage finished + Footer height adjusted

--- src/app/homepage/homepage/homepage.component.ts
+++ src/app/homepage/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss',
 })
-export class HomepageComponent {}
+export class HomepageComponent {
+  title = 'Raúl Soto';
+  subtitle = 'Information Systems Engineer / Software Developer';
+}
